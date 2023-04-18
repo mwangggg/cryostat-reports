fd957fee1f06240f4839dbe480d35459f1606002
Andrew Azores <aazores@redhat.com>
2023-04-18 10:54:47 -0400

    fix(run): update environment variable name (#101)

--- run.sh
+++ run.sh
@@ -30,6 +30,6 @@ podman run \
     --cpus "${CPUS}" \
     --memory "${MEMORY}" \
     --publish 8080:8080 \
-    --env JAVA_OPTIONS="-XX:ActiveProcessorCount=${CPUS} -XX:+PrintCommandLineFlags -Dorg.openjdk.jmc.flightrecorder.parser.singlethreaded=${SINGLETHREAD_JFR_PARSE} -Dio.cryostat.reports.memory-factor=${MEMORY_FACTOR} -Dio.cryostat.reports.timeout=${TIMEOUT}" \
+    --env JAVA_OPTS="-XX:ActiveProcessorCount=${CPUS} -XX:+PrintCommandLineFlags -Dorg.openjdk.jmc.flightrecorder.parser.singlethreaded=${SINGLETHREAD_JFR_PARSE} -Dio.cryostat.reports.memory-factor=${MEMORY_FACTOR} -Dio.cryostat.reports.timeout=${TIMEOUT}" \
     --rm -it \
     quay.io/cryostat/cryostat-reports:latest
